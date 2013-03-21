ed6dcb69c6c11ccb2970821a8eb03b4802ccfe35
AL30J11 <davidoct26@gmail.com>
2013-03-21 07:43:07 -0600

    version 1.2 funciones.js

--- js/acciones.js
+++ js/acciones.js
@@ -27,5 +27,17 @@ $(document).ready(  function(e){
 		$('#derecha').swiperight(function(){
 			navigator.notification.alert("Desliz&oacute; a la derecha", function(){},"Practica 1", "Aceptar");
 		});
+		$('#izquierda').swipeleft(function(){
+			navigator.notification.confirm('¿Qu&eacute; desea hacer?', function(opcion){
+				switch (opcion){
+					case 1:
+						navigator.notification.beep(1);
+					break;
+					case 2:
+						navigator.notification.vivrate(500);
+					break;
+				} //switch
+			}, "Practica 1", "beep, vibrar, cancelar"); //function confirm
+		}); //function swipe
 	}, false);
 });
