e57ee4c3e088b0d1346a0b0cb79a30569ed5278e
AL30J11 <davidoct26@gmail.com>
2013-03-21 08:06:24 -0600

    version 1.2.1 acciones.js

--- js/acciones.js
+++ js/acciones.js
@@ -25,16 +25,16 @@ $(document).ready(  function(e){
 			}
 		});
 		$('#derecha').swiperight(function(){
-			navigator.notification.alert("Desliz&oacute; a la derecha", function(){},"Practica 1", "Aceptar");
+			navigator.notification.alert("Deslizó a la derecha", function(){},"Practica 1", "Aceptar");
 		});
 		$('#izquierda').swipeleft(function(){
-			navigator.notification.confirm('¿Qu&eacute; desea hacer?', function(opcion){
+			navigator.notification.confirm('¿Qué desea hacer?', function(opcion){
 				switch (opcion){
 					case 1:
 						navigator.notification.beep(1);
 					break;
 					case 2:
-						navigator.notification.vivrate(500);
+						navigator.notification.vibrate(500);
 					break;
 				} //switch
 			}, "Practica 1", "beep, vibrar, cancelar"); //function confirm
